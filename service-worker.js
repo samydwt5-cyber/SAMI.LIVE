8d34be61be41928057456edac3ecb0f81d8c5ea4
samydwt5-cyber <samydwt5@gmail.com>
2026-09-21 13:27:03 +0300

    Upgrade service worker to version 2 and enhance caching
    Updated service worker version and improved caching strategy.

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const V='sami-life-v1',CORE=['./','index.html','manifest.json','icons/icon-192.png','icons/icon-512.png','icons/icon-maskable-512.png','icons/apple-touch-icon.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(V).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
+const V='sami-life-v2',CORE=['./','index.html','manifest.json','icon-192.png','icon-512.png','icon-maskable-512.png','apple-touch-icon.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(V).then(c=>Promise.all(CORE.map(u=>c.add(u).catch(()=>{})))).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==V).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
   const r=e.request;if(r.method!=='GET')return;const u=new URL(r.url);
